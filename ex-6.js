c9f965a8134b0fc1de2ef4de8b8daf1c0cc8ccc8
Natchanon <nutchanoon.yen@gmail.com>
2026-06-16 14:04:44 +0700

    feat: add answer in ex-1 to ex-7

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,9 @@
 let height = undefined;
 let result;
 
+function checkHeightValue (height) {
+    return height ?? `Height is not defined`;
+}
+
+result = checkHeightValue(height);
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
